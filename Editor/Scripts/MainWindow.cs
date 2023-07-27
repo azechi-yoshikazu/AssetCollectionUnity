97a42db0679d3170816720795aae1e929ab64dca
Yoshikazu <walkingcat.cute@gmail.com>
2023-07-28 02:35:07 +0900

    feature: added namespace

---
+++ Editor/Scripts/MainWindow.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AssetCollection
+{
+    public class AssetCollectionWindow : EditorWindow
+    {
+        private DragAndDropManipulator _dragAndDropManipulator;
+
+        private ListView _assetList;
+        private List<string> _assetListSource = new();
+
+        [MenuItem("Window/AssetCollection")]
+        public static void ShowWindow()
+        {
+            GetWindow<AssetCollectionWindow>("AssetCollection");
+        }
+
+        private void OnEnable()
+        {
+            var tree = Resources.Load<VisualTreeAsset>("MainWindow");
+
+            tree.CloneTree(rootVisualElement);
+
+            var dropArea = rootVisualElement.Q<VisualElement>("DropArea");
+            _dragAndDropManipulator = new(dropArea);
+            _dragAndDropManipulator.DroppedAction += OnDropped;
+
+            var assetList = rootVisualElement.Q<ListView>("AssetList");
+            assetList.makeItem = () => new Label();
+            assetList.bindItem = (element, index) => (element as Label).text = _assetListSource[index];
+            assetList.itemsSource = _assetListSource;
+            assetList.itemsSourceChanged += () => Debug.Log("itemsSourceChanged");
+            assetList.itemsChosen += OnItemChosen;
+
+            _assetList = assetList;
+        }
+
+        private void OnItemChosen(IEnumerable<object> itemObject)
+        {
+            var path = itemObject.First() as string;
+            var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
+            EditorGUIUtility.PingObject(obj);
+        }
+
+        private void OnDropped()
+        {
+            Debug.Log("OnDropped");
+            foreach (var path in DragAndDrop.paths)
+            {
+                if (!_assetListSource.Contains(path))
+                {
+                    _assetListSource.Add(path);
+                }
+            }
+            _assetListSource.Sort();
+
+            _assetList.Rebuild();
+        }
+
+        private void OnDisable()
+        {
+            _dragAndDropManipulator.Dispose();
+        }
+    }
+
+};
